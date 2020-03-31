dccfe62d9d8bda9ef4aafb8b265b9c32a0adc76e
irmantasdv <irmantas.dv@gmail.com>
2020-03-31 12:58:25 +0300

    first commit

---
+++ src/main/java/com/learningSpring/SpringEmbeddedClassApplication.java
@@ -0,0 +1,13 @@
+package com.learningSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmbeddedClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmbeddedClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningSpring/SpringEmbeddedClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmbeddedClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
